137486245a7c511c815c27c4e188da3789265c5f
ernstsamaniego <ernstsamaniego@yahoo.com>
2019-10-22 08:45:47 -0400

    fixed formula output

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,38 +1,43 @@
-$(document).ready(function () {
-    // toggles the state of the popover text
-    $(function () {
-        $('[data-toggle="popover"]').popover();
-    });
-  
-  
-  $('.input-group-text').on('click', function(){
-    $(this).addClass('line-through')
+$(document).ready(function() {
+  // toggles the state of the popover text
+  $(function() {
+    $('[data-toggle="popover"]').popover();
+  });
+
+
+  $('ul').on('click', 'li', function() {
+    $(this).toggleClass('completed')
   })
 
-// chol calculation
+  // chol calculation
 
-let cholCalc = $('#chol-cal');
+  let cholCalc = $('#chol-cal');
 
-cholCalc.on('click', (e) => {
+  cholCalc.on('click', (e) => {
     e.preventDefault();
-    let chol = $('#chol-input').val();
-    let hdl = $('#hdl-input').val();
-    let trig = $('#trig-input').val();
-  
+    let chol = parseFloat($('#chol-input').val());
+    let hdl = parseFloat($('#hdl-input').val());
+    let trig = parseFloat($('#trig-input').val());
+
 
     // formula chol - (trig / 5 + HDL) 194-(31.66 + 82.1) = 80.24
-   let totalCalculation = chol - hdlCalculation;
 
-   let trigCalculation = trig / 5;
 
-   let hdlCalculation = trigCalculation + hdl;
+    let trigCalculation = trig / 5;
+    let totalParens = trigCalculation + hdl;
+    console.log('total parens ' + totalParens);
 
-    // make variables here for calculations
+    let cholEquation = chol - totalParens;
+    console.log('Chol Calc: ' + cholEquation);
+    
+    
+      $('#cholCalculation').html('LDL Chol Calc: ' + cholEquation)
+   
 
 
     // if statement here for empty inputs
 
-});
+  });
 
 
 });
