6f2cd12ffd226d0f29be71709591ba99fcc5fe85
Ernesto Samaniego <esamaniego53@gmail.com>
2019-11-03 22:25:46 +0000

    tidy up code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,45 +1,45 @@
 $(document).ready(function() {
-  // toggles the state of the popover text
-  $(function() {
-    $('[data-toggle="popover"]').popover();
-  });
+    // toggles the state of the popover text
+    $(function() {
+        $('[data-toggle="popover"]').popover();
+    });
 
 
-//   $('ul').on('click', '.input-group-text', function() {
-//     $(this).toggleClass('completed')
-//   })
+    //   $('ul').on('click', '.input-group-text', function() {
+    //     $(this).toggleClass('completed')
+    //   })
 
-  // chol calculation
+    // chol calculation
 
-  let cholCalc = $('#chol-cal');
+    let cholCalc = $('#chol-cal');
 
-  cholCalc.on('click', (e) => {
-    e.preventDefault();
-    let chol = parseFloat($('#chol-input').val());
-    let hdl = parseFloat($('#hdl-input').val());
-    let trig = parseFloat($('#trig-input').val());
-
-
-    // formula chol - (trig / 5 + HDL) 194-(31.66 + 82.1) = 80.24
+    cholCalc.on('click', (e) => {
+        e.preventDefault();
+        let chol = parseFloat($('#chol-input').val());
+        let hdl = parseFloat($('#hdl-input').val());
+        let trig = parseFloat($('#trig-input').val());
 
 
-    let trigCalculation = trig / 5;
-    let totalParens = trigCalculation + hdl;
-    console.log('total parens ' + totalParens);
-
-    let cholEquation = chol - totalParens;
-    console.log('Chol Calc: ' + cholEquation);
+        // formula chol - (trig / 5 + HDL) 194-(31.66 + 82.1) = 80.24
 
 
-    if (isNaN(chol) || chol === "" || isNaN(hdl) || hdl === "" || isNaN(trig) || trig === "") {
-      $('#cholCalculation').html("<h5>" + "Error, All fields required!!" + "</h5>");
+        let trigCalculation = trig / 5;
+        let totalParens = trigCalculation + hdl;
+        console.log('total parens ' + totalParens);
 
-    } else {
+        let cholEquation = chol - totalParens;
+        console.log('Chol Calc: ' + cholEquation);
 
-      $('#cholCalculation').html('LDL Chol Calc: ' + cholEquation);
-    }
 
-  });
+        if (isNaN(chol) || chol === "" || isNaN(hdl) || hdl === "" || isNaN(trig) || trig === "") {
+            $('#cholCalculation').html("<h5>" + "Error, All fields required!!" + "</h5>");
+
+        } else {
+
+            $('#cholCalculation').html('LDL Chol Calc: ' + cholEquation);
+        }
+
+    });
 
 
 });
